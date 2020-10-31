7991eb6750e3e0f979920bf0bccce913c226843d
Thanh Luong Giang <Len_giang@yahoo.de>
2020-10-31 17:39:35 +0100

    add crawler function to main

--- Covid-19WorldInfo/src/hft/cwi/main/Main.java
+++ Covid-19WorldInfo/src/hft/cwi/main/Main.java
@@ -1,8 +1,24 @@
 package hft.cwi.main;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import hft.cwi.etl.crawler.who.WHOCrawler;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// empty
+	private static String _pdfURL = "https://apps.who.int/iris/bitstream/handle/10665/331917/COVID-19-infection-prevention-during-transfer-and-transport-eng.pdf?sequence=1&isAllowed=y";
+
+	private static String _htmlURL = "https://www.who.int";
+
+	public static void main(String[] args) throws IOException {
+		WHOCrawler crawler = new WHOCrawler();
+		try {
+			crawler.startCrawling(new URL(_pdfURL), null);
+			crawler.startCrawling(new URL( _htmlURL), null);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
 }
