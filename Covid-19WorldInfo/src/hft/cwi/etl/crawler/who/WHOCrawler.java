cf738e6cfe9b8d07117742cdca93b4d7b6d631dc
Thanh Luong Giang <Len_giang@yahoo.de>
2020-10-31 17:39:27 +0100

    implement WHOCrawler, it can crawl through 1 pdf or 1 html  site

--- Covid-19WorldInfo/src/hft/cwi/etl/crawler/who/WHOCrawler.java
+++ Covid-19WorldInfo/src/hft/cwi/etl/crawler/who/WHOCrawler.java
@@ -1,6 +1,7 @@
 package hft.cwi.etl.crawler.who;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
@@ -13,32 +14,42 @@ import hft.cwi.etl.crawler.Crawler;
 import hft.cwi.etl.crawler.ICrawler;
 import hft.cwi.etl.crawler.WebpageData;
 import hft.cwi.etl.filehandling.HTMLHandling;
+import hft.cwi.etl.filehandling.PDFHandling;
 
-public class WHOCrawler extends Crawler implements ICrawler	{
+public class WHOCrawler extends Crawler implements ICrawler {
 
-	public static final String START_URL =  "https://apps.who.int/iris/sitemap";
-	
+	public static final String START_URL = "https://apps.who.int/iris/sitemap";
+
 	private static final int MAX_AMOUNTS_OF_URL_TO_VISIT = 1;
-	
+
 	Collection<WebpageData> _allWebpages = new ArrayList<>();
-	
+
 	Set<URI> _alreadyVisitedURL = new HashSet<>();
-	
+
 	@Override
 	public void startCrawling(URL startURL, Collection<String> keywordsToLookOutFor) {
 		try {
 			URLConnection urlConnection = startURL.openConnection();
-			if(isXMLFile(urlConnection)) {
+			if (isXMLFile(urlConnection)) {
 				
 			} else if (isHTMLFile(urlConnection)) {
-				
-			} else if(isPDFFile(urlConnection)) {
-				
+				HTMLHandling.getAllURLFromHTML(startURL.toString()) //
+						.stream() //
+						.forEach(this::collectAllLinks);
+				_allWebpages.forEach(webpages -> System.out.println(webpages.getWebpage().toString()));
+			} else if (isPDFFile(urlConnection)) {
+				System.out.println(PDFHandling.getRawPDFData(startURL.openStream()));
 			}
-			
-			System.out.println(urlConnection.getContentType());
-			
+
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void collectAllLinks(URI uri){
+		try {
+			_allWebpages.add(new WebpageData(uri.toURL()));
+		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 	}
@@ -47,5 +58,5 @@ public class WHOCrawler extends Crawler implements ICrawler	{
 	public Collection<WebpageData> getAllCrawlerData() {
 		return null;
 	}
-	
+
 }
