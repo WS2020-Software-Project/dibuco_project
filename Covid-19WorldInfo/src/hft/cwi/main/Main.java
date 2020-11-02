8d9ac312886a89f1d3890524a26641a64065cb9e
Thanh Luong Giang <Len_giang@yahoo.de>
2020-11-02 21:04:50 +0100

    add properties reader to main method

--- Covid-19WorldInfo/src/hft/cwi/main/Main.java
+++ Covid-19WorldInfo/src/hft/cwi/main/Main.java
@@ -1,38 +1,40 @@
 package hft.cwi.main;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Properties;
 
-import hft.cwi.etl.crawler.ncdc.NCDCCrawler;
-import hft.cwi.etl.crawler.rki.RKICrawler;
-import hft.cwi.etl.crawler.who.WHOCrawler;
+import hft.cwi.etl.crawler.CrawlerController;
 
 public class Main {
 
-	private static String _pdfURL = "https://apps.who.int/iris/bitstream/handle/10665/331917/COVID-19-infection-prevention-during-transfer-and-transport-eng.pdf?sequence=1&isAllowed=y";
-
-	private static String _htmlURL = "https://www.who.int";
-	
-	private static String _xmlURL = "https://www.who.int/sitemaps/sitemapindex.xml";
-	
-	private static String _ncdcURL = "https://www.ncdc.gov.in";
-
 	public static void main(String[] args) throws IOException {
-		WHOCrawler whoCrawler = new WHOCrawler();
-		RKICrawler rkiCrawler = new RKICrawler();
-		NCDCCrawler ncdcCrawler = new NCDCCrawler();
-		try {
-			System.out.println("Enea was here!!!!");
-//			ncdcCrawler.startCrawling(new URL(_ncdcURL), null);
-//			whoCrawler.startCrawling(new URL(_pdfURL), null);
-//			whoCrawler.startCrawling(new URL( _htmlURL), null);
-//			//xml crawler over WHO wesite
-			whoCrawler.startCrawling(new URL( _xmlURL), null);
-//			
-//			rkiCrawler.startCrawling(new URL(RKICrawler.START_URL), null);
+
+		try (InputStream inputStream = new FileInputStream("resources/crawler.properties")) {
+			Properties properties = new Properties();
+			properties.load(inputStream);
+			Collection<String> startEntryWebpages = Arrays
+					.asList(properties.getProperty("crawler.entrypage").split(","));
+			CrawlerController crawler = new CrawlerController();
+			startEntryWebpages.stream() //
+					.map(Main::createURLFromString) //
+					.forEach(startUrl -> crawler.startCrawling(startUrl, null));
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
+		}
+	}
+
+	public static URL createURLFromString(String urlAsString) {
+		try {
+			return new URL(urlAsString);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 }
