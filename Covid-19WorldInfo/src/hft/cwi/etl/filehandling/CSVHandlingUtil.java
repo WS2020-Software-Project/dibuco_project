d8ab9a8c14b8952c0931ff8bf1492d28b56b7a7d
Thanh Luong Giang <Len_giang@yahoo.de>
2020-11-05 13:55:54 +0100

    simplified code, remove duplicated code, change fileoutput to txt

--- Covid-19WorldInfo/src/hft/cwi/etl/filehandling/CSVHandlingUtil.java
+++ Covid-19WorldInfo/src/hft/cwi/etl/filehandling/CSVHandlingUtil.java
@@ -15,70 +15,74 @@ import hft.cwi.etl.crawler.WebpageData;
 
 public class CSVHandlingUtil {
 	
+	private static final String TEMPORARY_FILE_NAME = "temporaryFile";
+
 	public static void writeCSVFile(Collection<WebpageData> webpageData) {
 		try (FileWriter csvWriter = new FileWriter("new.csv")){
 			createCSVHeaderFile(csvWriter);
-
-			webpageData.stream().forEach(webpage -> {
-				try {
-					URLConnection urlConnection = webpage.getWebpage().openConnection();
-					DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-			        Date date = new Date(urlConnection.getDate());
-					csvWriter.append(dateFormat.format(date));
-					csvWriter.append(",");
-					if(urlConnection.getContentType().contains("html")
-							|| urlConnection.getContentType().contains("plain")) {
-						csvWriter.append("html");
-						csvWriter.append(",");
-						File file = createFile("temporaryFile",".html");
-						FileWriter csvWriter2 = new FileWriter(file);
-						csvWriter2.append(webpage.getWebPageContent());
-						csvWriter2.flush();
-						csvWriter2.close();
-						addDataToCSVFile(csvWriter, webpage, file);
-					} else if(urlConnection.getContentType().contains("text/xml") ||
-							urlConnection.getURL().toString().contains(".xml")) {
-						csvWriter.append("xml");
-						csvWriter.append(",");
-						File file = createFile("temporaryFile",".xml");
-						FileUtils.copyInputStreamToFile(webpage.getWebpage().openConnection().getInputStream(), file);
-						addDataToCSVFile(csvWriter, webpage, file);
-					} else if(urlConnection.getContentType().contains("pdf")) {
-						csvWriter.append("pdf");
-						csvWriter.append(",");
-						File file = createFile("temporaryFile",".pdf");
-						FileUtils.copyInputStreamToFile(webpage.getWebpage().openConnection().getInputStream(), file);
-						addDataToCSVFile(csvWriter, webpage, file);
-					} else {
-						csvWriter.append("other");
-						csvWriter.append(",");
-						File file = createFile("temporaryFile",".txt");
-						FileUtils.copyInputStreamToFile(webpage.getWebpage().openConnection().getInputStream(), file);
-						addDataToCSVFile(csvWriter, webpage, file);
-					}
-					csvWriter.append("\n");
-					System.out.println("append a line to csv");
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			});
+			webpageData.stream() //
+				.forEach(webpage -> createCSVFile(csvWriter, webpage));
 			csvWriter.flush();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	private static File createFile(String prefix, String suffix) throws IOException {
+	private static void createCSVFile(FileWriter csvWriter, WebpageData webpage) {
+		try {
+			URLConnection urlConnection = webpage.getWebpage().openConnection();
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		    Date date = new Date(urlConnection.getDate());
+			csvWriter.append(dateFormat.format(date));
+			csvWriter.append(",");
+			if(isHTMLFile(urlConnection)) {
+				File file = createFile(TEMPORARY_FILE_NAME,".txt", webpage.getWebPageContent());
+				addDataToCSVFile("html",csvWriter, webpage, file);
+			} else if(isXMLFile(urlConnection)) {
+				File file = createFile(TEMPORARY_FILE_NAME,".txt",webpage.getWebPageContent());
+				addDataToCSVFile("xml",csvWriter, webpage, file);
+			} else if(isPDFFile(urlConnection)) {
+				File file = createFile(TEMPORARY_FILE_NAME,".txt",webpage.getWebPageContent());
+				addDataToCSVFile("pdf",csvWriter, webpage, file);
+			} else {
+				File file = createFile(TEMPORARY_FILE_NAME,".txt",webpage.getWebPageContent());
+				addDataToCSVFile("other",csvWriter, webpage, file);
+			}
+			csvWriter.append("\n");
+			System.out.println("append a line to csv");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static boolean isPDFFile(URLConnection urlConnection) {
+		return urlConnection.getContentType().contains("pdf");
+	}
+
+	private static boolean isXMLFile(URLConnection urlConnection) {
+		return urlConnection.getContentType().contains("text/xml") ||
+				urlConnection.getURL().toString().contains(".xml");
+	}
+
+	private static boolean isHTMLFile(URLConnection urlConnection) {
+		return urlConnection.getContentType().contains("html")
+				|| urlConnection.getContentType().contains("plain");
+	}
+
+	private static File createFile(String prefix, String suffix, String webPageContent) throws IOException {
 		File file = File.createTempFile(prefix, suffix);
 		file.deleteOnExit();
+		try(FileWriter localFileWriter = new FileWriter(file)) {
+			localFileWriter.append(webPageContent);
+		}
 		return file;
 	}
 
-	private static void addDataToCSVFile(FileWriter csvWriter, WebpageData webpage, File file) throws IOException {
+	private static void addDataToCSVFile(String docType, FileWriter csvWriter, WebpageData webpage, File file) throws IOException {
 		URLConnection urlConnection = webpage.getWebpage().openConnection();
-		csvWriter.append(file.getName());
+		csvWriter.append(docType);
+		csvWriter.append(",");
+		csvWriter.append(file.getAbsolutePath());
 		csvWriter.append(",");
 		webpage.getAllKeywords().entrySet().forEach(entry -> {
 			try {
