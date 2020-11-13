7139cfeae53be6cb40f393b12986a3bf9aa4c6bc
Thanh Luong Giang <Len_giang@yahoo.de>
2020-11-13 09:56:03 +0100

    clean up, split lang test into multiple test

--- Covid-19WorldInfo/src/hft/cwi/etl/languagedetection/LangDetector.java
+++ Covid-19WorldInfo/src/hft/cwi/etl/languagedetection/LangDetector.java
@@ -6,13 +6,22 @@ import com.cybozu.labs.langdetect.LangDetectException;
 
 public class  LangDetector {
 	
-    public void init(String profileDirectory) throws LangDetectException {
-        DetectorFactory.loadProfile(profileDirectory);
-      
-    }
-    public String detect(String text) throws LangDetectException {
-        Detector detector = DetectorFactory.create();
-        detector.append(text);
-        return detector.detect();
+	public LangDetector(String profileDirectory) {
+		 try {
+			DetectorFactory.loadProfile(profileDirectory);
+		} catch (LangDetectException e) {
+			e.printStackTrace();
+		}
+	}
+    public String detect(String text) {
+        Detector detector;
+		try {
+			detector = DetectorFactory.create();
+		    detector.append(text);
+	        return detector.detect();
+		} catch (LangDetectException e) {
+			return "undefined languange";
+		}
+    
     }
 }
--- Covid-19WorldInfo/src/hft/cwi/etl/languagedetection/LangDetectorTest.java
+++ Covid-19WorldInfo/src/hft/cwi/etl/languagedetection/LangDetectorTest.java
@@ -1,27 +1,35 @@
 package hft.cwi.etl.languagedetection;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.cybozu.labs.langdetect.LangDetectException;
+public class LangDetectorTest {
 
-class LangDetectorTest {
+	private static LangDetector _ld;
+
+	@BeforeAll
+	public static void init() {
+		_ld = new LangDetector("profiles");
+	}
 
 	@Test
-	void test() {
-		LangDetector ld= new LangDetector();
-		try {
-			ld.init("profiles");
-			assert(ld.detect("This is an English language test!").equalsIgnoreCase("en") &&
-					ld.detect("هذا اختبار لغة عربية!").equalsIgnoreCase("ar") &&
-					ld.detect("Das ist ein deutscher Sprachtest!").equalsIgnoreCase("de") &&
-					ld.detect("Ceci est un test de langue française!").equalsIgnoreCase("fr"));
-		} catch (LangDetectException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail();
-		}
+	public void testENLanguange() {
+		assert (_ld.detect("This is an English language test!").equalsIgnoreCase("en"));
+	}
+
+	@Test
+	public void testARLanguange() {
+		assert (_ld.detect("هذا اختبار لغة عربي!").equalsIgnoreCase("ar"));
+	}
+
+	@Test
+	public void testDELanguange() {
+		assert (_ld.detect("Das ist ein deutscher Sprachtest!").equalsIgnoreCase("de"));
+	}
+
+	@Test
+	public void testFRLanguange() {
+		assert (_ld.detect("Ceci est un test de langue franÃ§aise!").equalsIgnoreCase("fr"));
 	}
 
 }
