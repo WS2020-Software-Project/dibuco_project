2d8ad25e70f16e3fba200ebb95d9e35134371345
Thanh Luong Giang <Len_giang@yahoo.de>
2020-11-15 13:13:06 +0100

    move robot file reader and robot delay code.
    adjust properties files

--- Covid-19WorldInfo/src/hft/cwi/etl/crawler/Crawler.java
+++ Covid-19WorldInfo/src/hft/cwi/etl/crawler/Crawler.java
@@ -1,5 +1,15 @@
 package hft.cwi.etl.crawler;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
 import org.jsoup.Connection.Response;
 
 public abstract class Crawler {
@@ -36,13 +46,34 @@ public abstract class Crawler {
 		return _crawlingDeepness <= deepness;
 	}
 
-	protected void waitUntilAccessNextWebsite() {
+	protected void delayCrawler() {
 		try {
 			Thread.sleep(_timeBufferInMs);
 		} catch (InterruptedException e) {
-			e.printStackTrace();
 			Thread.currentThread().interrupt();
 		}
+	}
+	
+	protected void readRobotFile(String propertiesFileName) {
+		try (InputStream inputStream = new FileInputStream("resources/" + propertiesFileName)){
+			Properties properties = new Properties();
+			properties.load(inputStream);		
+			
+			String url = properties.getProperty("crawler.robot");
+			if(url.isEmpty()) {
+				return;
+			}
+			
+			try (BufferedReader in = new BufferedReader(new InputStreamReader(new URL( url).openStream()))){
+				String line = null;
+		        while((line = in.readLine()) != null) {
+		            System.out.println(line);
+		        }
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
 	}
 
 }
