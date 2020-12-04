ebf97556997ec3466306275c44e3d01392df6e67
Thanh Luong Giang <Len_giang@yahoo.de>
2020-12-04 21:27:24 +0100

    change crawler behavior, simplify code remove obsole code

--- Covid-19WorldInfo/src/hft/cwi/etl/crawler/ICrawler.java
+++ Covid-19WorldInfo/src/hft/cwi/etl/crawler/ICrawler.java
@@ -1,14 +1,9 @@
 package hft.cwi.etl.crawler;
 
-import java.net.URI;
 import java.util.Collection;
-import java.util.Set;
 
 public interface ICrawler {
 
 	public void startCrawling(Collection<String> keywordsToLookOutFor);
-	
-	public Collection<WebpageData> getAllCrawlerData();
-	
 	
 }
--- Covid-19WorldInfo/src/hft/cwi/etl/crawler/ncdc/NCDCCrawler.java
+++ Covid-19WorldInfo/src/hft/cwi/etl/crawler/ncdc/NCDCCrawler.java
@@ -1,37 +1,18 @@
 package hft.cwi.etl.crawler.ncdc;
 
-import java.io.IOException;
 import java.net.URI;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-
-import org.jsoup.Connection;
-import org.jsoup.Connection.Response;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 import hft.cwi.etl.crawler.Crawler;
 import hft.cwi.etl.crawler.CrawlerPropertiesFilesReader;
 import hft.cwi.etl.crawler.CrawlerSeed;
 import hft.cwi.etl.crawler.ICrawler;
-import hft.cwi.etl.crawler.WebpageData;
-import hft.cwi.etl.filehandling.HTMLHandlingUtil;
-import hft.cwi.etl.filehandling.PDFHandlingUtil;
 
 public class NCDCCrawler extends Crawler implements ICrawler{
 	
-
-	private static Collection<WebpageData> _allWebpages = new ArrayList<>();
-
 	private static Collection<String> _forbiddenURI //
 			= CrawlerPropertiesFilesReader.getForbiddenURL("ncdccrawlersettings.properties");
-
-	private static Set<URI> _websiteToVisit = new HashSet<>();
 
 	private URI _startURI;
 
@@ -43,32 +24,13 @@ public class NCDCCrawler extends Crawler implements ICrawler{
 	@Override
 	public void startCrawling(Collection<String> keywordsToLookOutFor) {
 		System.out.println("start crawling in NCDC .....");
-		
-		final List<CrawlerSeed> theSeedList = new ArrayList<>();
+		List<CrawlerSeed> theSeedList = new ArrayList<>();
 		theSeedList.add(0, new CrawlerSeed(_startURI, 1));
-		_websiteToVisit.addAll(collectWebsiteURIs(theSeedList));
-		
-		_websiteToVisit.forEach(uri -> System.out.println(uri.toString()));
-		 writeOnFile(_websiteToVisit); 
-		 readURISFromFile();
-		 _alreadyVisitedWebsites.clear();
-	}
-
-
-	protected void collectAllLinks(URI uri, String webPageContent, String docType, URLConnection urlConnection) {
-		System.out.println("\n start collecting Weppage data .....\n the content is "+docType);
-		
-		_allWebpages.add(new WebpageData(uri, webPageContent,docType,urlConnection));
-	}
-
-	@Override
-	public Collection<WebpageData> getAllCrawlerData() {
-		return _allWebpages;
+		collectWebsiteURIs(theSeedList);
 	}
 
 	protected boolean isForbiddenLink(String uriAsString) {
 		if (_forbiddenURI.stream().anyMatch(uriAsString::contains)) {
-			System.out.println("remove link " + uriAsString);
 		}
 		return _forbiddenURI.stream().anyMatch(uriAsString::contains);
 	}
