1bddb0532702ba1ee9960a030e6d79b38abeb316
Thanh Luong Giang <Len_giang@yahoo.de>
2020-12-08 22:37:38 +0100

    export lang profiles out of jar due to misshandling of the 3rd party software

--- Covid-19WorldInfo/src/hft/cwi/etl/languagedetection/LangDetector.java
+++ Covid-19WorldInfo/src/hft/cwi/etl/languagedetection/LangDetector.java
@@ -1,15 +1,41 @@
 package hft.cwi.etl.languagedetection;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.Collection;
+
 import com.cybozu.labs.langdetect.Detector;
 import com.cybozu.labs.langdetect.DetectorFactory;
 import com.cybozu.labs.langdetect.LangDetectException;
 
 public class  LangDetector {
 	
+	private static Collection<String> _allProfilesName = Arrays.asList("af","ar","bg","bn","cs","da","de","el","en","es"
+			,"et","fa","fi","fr","gu","he","hi","hr","hu","id","it","ja","kn","ko","lt","lv","mk","ml","mr","ne","nl","no","pa","pl","pt","ro"
+			,"ru","sk","sl","so","sq","sv","sw","ta","te","th","tl","tr","uk","ur","vi","zh-cn","zh-tw");
+	
 	public LangDetector(String profileDirectory) {
 		 try {
+			File directory = new File(profileDirectory);
+			if(!directory.exists()) {
+				directory.mkdir();
+				_allProfilesName.stream().forEach(name -> createProfile(profileDirectory, name));
+			}
 			DetectorFactory.loadProfile(profileDirectory);
 		} catch (LangDetectException e) {
+			e.printStackTrace();
+		}
+	}
+	private void createProfile(String profileDirectory, String name) {
+		File targetFile = new File(profileDirectory + "/" + name);
+		try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(name)){
+			Files.copy(inputStream, targetFile.toPath(),StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
